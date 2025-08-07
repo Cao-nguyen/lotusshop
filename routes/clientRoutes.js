3e544c9a14128d298d091426c974a6d5ea2b101a
Cao-nguyen <chinhphucnguvan@gmail.com>
2025-08-07 15:16:54 +0700

    push

--- routes/clientRoutes.js
+++ routes/clientRoutes.js
@@ -6,4 +6,12 @@ router.get('/', (req, res) => {
     res.render('./client/home/home.pug', { title: "Trang chủ" })
 })
 
+router.get('/gioithieu', (req, res) => {
+    res.render('./client/introduce/introduce.pug', { title: "Giới thiệu" })
+})
+
+router.get('/sanpham', (req, res) => {
+    res.render('./client/products/product.pug', { title: "Sản phẩm" })
+})
+
 module.exports = router
